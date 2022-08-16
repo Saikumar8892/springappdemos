c53db5edad62231391cfcf4e5213a2016b120222
github_Saikumar8892 <sairoyalsulli@gmail.com>
2022-08-16 13:03:49 +0530

    changes

--- S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
+++ S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
@@ -1,13 +1,24 @@
 package com.samples.S01springcoredi;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+       @SuppressWarnings("resource")
+	ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext("com/samples/S01springcoredi/SpringConfig.xml");
+       
+      // Employee emp = (Employee) springContainer.getBean("emp"); 
+      // System.out.println(emp);
+       
+    //Hospital hospital = (Hospital) springContainer.getBean("hospital"); 
+      //  System.out.println(hospital);
+       
+       //CarDealer dealer = (CarDealer) springContainer.getBean("cardealer");
+       //System.out.println(dealer);
+       
+       Customer customer = (Customer) springContainer.getBean("customer");
+       System.out.println(customer);
     }
 }
